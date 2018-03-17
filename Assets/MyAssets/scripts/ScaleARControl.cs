a196f51c9ea41a292ae634be71f4c2e80cd17e02
Nishant Kumar <niskumar@adobe.com>
2018-03-17 12:57:01 +0530

    Auto resize of AR components

---
+++ Assets/MyAssets/scripts/ScaleARControl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleARControl : MonoBehaviour {
+	public GameObject chair;
+	public GameObject table;
+	public GameObject lamp;
+	public GameObject cabinet;
+
+	private bool chairButtonClicked = false;
+	private bool tableButtonClicked = false;
+	private bool lampButtonClicked = false;
+	private bool cabinetButtonClicked = false;
+
+
+	// Use this for initialization
+	void Start () {
+		chair.SetActive (false);
+		lamp.SetActive (false);
+		cabinet.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		chair.SetActive (chairButtonClicked);
+		lamp.SetActive (lampButtonClicked);
+		cabinet.SetActive (cabinetButtonClicked);
+	}
+
+	public void HandleIncreaseScale() {
+		table.transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
+		chair.transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
+		lamp.transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
+		chair.transform.Translate (0.0f, 0.0f, 0.001f, Space.World);
+		table.transform.Translate (0.0f, 0.0f, 0.001f, Space.World);
+		lamp.transform.Translate (0.03f, 0.05f, 0.0f, Space.World);
+	}
+
+	public void HandleDecreaseScale() {
+		table.transform.localScale -= new Vector3 (0.1F, 0.1F, 0.1F);
+		chair.transform.localScale -= new Vector3 (0.1F, 0.1F, 0.1F);
+		lamp.transform.localScale -= new Vector3 (0.1F, 0.1F, 0.1F);
+		chair.transform.Translate (0.0f, 0.0f, -0.001f, Space.World);
+		table.transform.Translate (0.0f, 0.0f, -0.001f, Space.World);
+		lamp.transform.Translate (-0.03f, -0.05f, 0.0f, Space.World);
+	}
+
+	public void HandlePlaceChair() {
+		chairButtonClicked = true;
+		chair.SetActive (true);
+	}
+
+	public void HandlePlaceTable(){
+		tableButtonClicked = true;
+		table.SetActive (true);
+	}
+
+	public void HandlePlaceLamp(){
+		lampButtonClicked = true;
+		lamp.SetActive (true);
+	}
+
+	public void HandlePlaceCabinet(){
+		cabinetButtonClicked = true;
+		cabinet.SetActive (true);
+	}
+}
